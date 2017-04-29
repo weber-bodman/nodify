a3fa165cfd84d7d9cf22d472380184c633047dd7
Maximilian Weber <post@wbrmx.de>
2017-04-29 18:21:00 +0200

    Add Basic functionality

---
+++ index.js
@@ -0,0 +1,43 @@
+var ws = require("nodejs-websocket");
+
+var clients = [];
+ 
+var wsServer = ws.createServer(function (conn) {
+    var clientId = '';
+    conn.on("text", function (str) {
+        clientId = str;
+        clients[clientId] = conn;
+        console.log("Client registered: " + clientId)
+    })
+    conn.on("close", function (code, reason) {
+        var index = clients.indexOf(clientId);
+        if (index > -1) {
+            clients.splice(index, 1);
+        }
+        console.log("Client released: " + clientId)
+    })
+}).listen(8001);
+
+var url = require('url');
+var http = require('http');
+
+var httpServer = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  var params = url.parse(request.url, true);
+
+  if(params.query && "client" in params.query && "payload" in params.query) {
+    var clientId = params.query.client;
+    var payload = params.query.payload;
+
+    if(clients[clientId] && payload) {
+        clients[clientId].sendText(payload);
+        console.log("Client nodified: " + clientId + " -> " + payload);
+        response.write("Client nodified: " + clientId + " -> " + payload + "\n");
+    }
+  }
+
+  response.end("Done \n");
+});
+
+httpServer.listen(8002);
